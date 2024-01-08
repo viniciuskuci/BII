5f4271763bb6ac0ca618b0f525d481ab3912cbb4
viniciuskuci <vinicius.kuci@hotmail.com>
2024-01-08 15:57:17 +0000

    Run simulation event separated to a new one.

--- SIMULATE.py
+++ SIMULATE.py
@@ -1,25 +1,32 @@
 import numpy as np
+import os
+import sys
 
-class SIMULATION:
+class SIMULATE:
     #the output is the throughput time of the system
 
     def __init__(self):
         
         self.cycle_times = []
         self.machine_info = {}
-        self.run = False
         self.simulation_time = 1440
     
-    def schedule(self, event_name, event_value, run_simulation, simulation_time, window, 
+    def schedule(self, event_name, event_value, simulation_time, window, 
                     station_1, station_2, station_3, station_4):
         
         if event_name == 'INIT':
-            self.run = run_simulation
             self.simulation_time = simulation_time
-            return [event_value, None, None]
+            current_dir = os.getcwd()
+            
+            try:
+                os.remove(os.path.join(current_dir,'simulation.py'))
+                print("simulation.py file removed.")
+            except:
+                pass
+
+            return [event_value, None, None, None]
         
         elif event_name == 'RUN':
-            
             input = [station_1, station_2, station_3, station_4]
             
             for i in range(len(input)):
@@ -36,36 +43,43 @@ class SIMULATION:
                     self.cycle_times[i].pop(0)
             
             #generate_simulation_file method needs the machines to be in order
-            self.machine_info = dict(sorted(self.machine_info.items()))    
+            self.machine_info = dict(sorted(self.machine_info.items()))             
+            
+            if len(self.machine_info) >= 2:
+                current_dir = os.getcwd()
+                try:
+                    file = open(os.path.join(current_dir,'simulation.py'), "r")
+                    file.close()
+                except:
+                    self.generate_simulation_file(self.machine_info, current_dir)
+            return [None, event_value, None, None]
 
+        elif event_name == 'RUN_SIM':
+            current_dir = os.getcwd()
             try:
-                file = open("simulation.py", "r")
-                file.close()
-                if self.run == True:
-                    import simulation
-                    throughput_time = simulation.execute_simulation(simulation.system, simulation.args)
-                    
-                    return [None, event_value, throughput_time]
-                else:
-                    return [None, event_value, None]
+                sys.path.append(current_dir)
+                import simulation
+                print(self.machine_info)
+                print("Running simulation...")
+                machine_cycle_times = [i[0] for i in self.machine_info.values()] #only considering the mean
+                throughput_time = simulation.execute_simulation(machine_cycle_times, self.simulation_time)
+                return [None, None, event_value, throughput_time]
             except:
-                if len(self.machine_info) >= 2:
-                    self.generate_simulation_file(self.machine_info)
-
-            return [None, event_value, None]
+                print("Unexpected error:", sys.exc_info())
+                return [None, None, event_value, None]
+               
+        return [None, event_value, None]
 
 
 
     @staticmethod 
-    def generate_simulation_file(machines_dict):
-
+    def generate_simulation_file(machines_dict, dir):
+        print("Generating simulation file...")
         machine_names = list(machines_dict.keys())
         machine_cycle_times = [i[0] for i in machines_dict.values()] #only considering the mean
-
-        file = open("simulation.py", "w")
+        file = open(os.path.join(dir,'simulation.py'), "w")
 
         file.write("import argparse\n")
-        file.write("from simantha import System, Machine, Source, Buffer, Sink\n")
         file.write("import numpy as np\n")
         file.write("\n")
         file.write("cycle_times = [" + ', '.join(map(str, machine_cycle_times)) + "]\n") 
@@ -74,35 +88,39 @@ class SIMULATION:
         file.write("parser.add_argument('-c', '--cycle_times', nargs='*', default=[], type=float, help='Machines cycle times')\n")
         file.write("parser.add_argument('-s', '--simulation_time', default=1440, type=float, help='Simulation time')\n")
         file.write("args = parser.parse_args()\n")
+        file.write("parsed_simulation_time = args.simulation_time\n")
+        file.write("parsed_cycle_times = args.cycle_times\n")
         file.write("\n")
-        file.write("if len(args.cycle_times) != 0:\n")
-        file.write("    cycle_times = args.cycle_times\n")
+        file.write("if len(parsed_cycle_times) == len(cycle_times):\n")
+        file.write("    cycle_times = parsed_cycle_times\n")
         file.write("\n")
-        file.write("S = Source()\n")
-        file.write("sink = Sink()\n")
+        file.write("def execute_simulation(cycle_times, parsed_simulation_time):\n")
+        file.write("    from simantha import System, Machine, Source, Buffer, Sink\n")
+        file.write("    S = Source()\n")
+        file.write("    sink = Sink()\n")
 
         for i in range(len(machine_names)):
-            file.write("M"+str(i+1)+" = Machine(name='"+machine_names[i]+"', cycle_time=dict(constant=cycle_times["+str(i)+"]))\n")
+            file.write("    M"+str(i+1)+" = Machine(name='"+machine_names[i]+"', cycle_time=dict(constant=cycle_times["+str(i)+"]))\n")
         file.write("\n")
         for i in range(len(machine_names)-1):
-            file.write("Queue_"+machine_names[i]+"_"+machine_names[i+1]+" = Buffer(name='Queue_"+machine_names[i]+"_"+machine_names[i+1]+"', capacity=10)\n")
+            file.write("    Queue_"+machine_names[i]+"_"+machine_names[i+1]+" = Buffer(name='Queue_"+machine_names[i]+"_"+machine_names[i+1]+"', capacity=10)\n")
         file.write("\n")
-        file.write("S.define_routing(downstream=[M1])\n")
+        file.write("    S.define_routing(downstream=[M1])\n")
         file.write("\n")
 
         for i in range(len(machine_names)):
             if i == 0:
-                file.write("M"+str(i+1)+".define_routing(upstream=[S], downstream=[Queue_"+machine_names[i]+"_"+machine_names[i+1]+"])\n")
+                file.write("    M"+str(i+1)+".define_routing(upstream=[S], downstream=[Queue_"+machine_names[i]+"_"+machine_names[i+1]+"])\n")
             elif i == len(machine_names)-1:
-                file.write("M"+str(i+1)+".define_routing(upstream=[Queue_"+machine_names[i-1]+"_"+machine_names[i]+"], downstream=[sink])\n")
+                file.write("    M"+str(i+1)+".define_routing(upstream=[Queue_"+machine_names[i-1]+"_"+machine_names[i]+"], downstream=[sink])\n")
             else:
-                file.write("M"+str(i+1)+".define_routing(upstream=[Queue_"+machine_names[i-1]+"_"+machine_names[i]+"], downstream=[Queue_"+machine_names[i]+"_"+machine_names[i+1]+"])\n")
+                file.write("    M"+str(i+1)+".define_routing(upstream=[Queue_"+machine_names[i-1]+"_"+machine_names[i]+"], downstream=[Queue_"+machine_names[i]+"_"+machine_names[i+1]+"])\n")
         file.write("\n")
 
         for i in range(len(machine_names)-1):
-            file.write("Queue_"+machine_names[i]+"_"+machine_names[i+1]+".define_routing(upstream=[M"+str(i+1)+"], downstream=[M"+str(i+2)+"])\n")
+            file.write("    Queue_"+machine_names[i]+"_"+machine_names[i+1]+".define_routing(upstream=[M"+str(i+1)+"], downstream=[M"+str(i+2)+"])\n")
         file.write("\n")
-        file.write("objects = [S, sink] + [")
+        file.write("    objects = [S, sink] + [")
 
         for i in range(len(machine_names)):
             if i == len(machine_names)-1:
@@ -113,24 +131,29 @@ class SIMULATION:
         for i in range(len(machine_names)-1):  
             file.write(", Queue_"+machine_names[i]+"_"+machine_names[i+1])
         file.write("]\n")
-        file.write("system = System(objects=objects)\n")
-        file.write("def execute_simulation(system, args):\n")
-        file.write("    system.simulate(simulation_time=args.simulation_time, verbose=False) #1440 minutes = 1 day\n")
-        file.write("    return args.simulation_time / sum(sink.level for sink in system.sinks) #returns the throughput time\n")
+        file.write("    system = System(objects=objects)\n")
+        file.write("    system.simulate(simulation_time=parsed_simulation_time, verbose=False)\n")
+        file.write("\n")
+        file.write("    return parsed_simulation_time / sum(sink.level for sink in system.sinks)\n")
         file.write("\n")
         file.write("if __name__ == '__main__':\n")
-        file.write("    print('Throughput time: ', execute_simulation(system, args))\n")
+        file.write("    print('Throughput time: ', execute_simulation(cycle_times, parsed_simulation_time))\n")
         file.close()
 
     
 #test:
         
 """
-a = SIMULATION()
-b = a.schedule('INIT', 0, True, 1440, 4, 0.25, 0.6, 0.32, 0.25)
-b = a.schedule('RUN', 0, True, 1440, 4, 0.25, 0.6, 0.32, 0.25)
-b = a.schedule('RUN', 0, True, 1440, 4, 0.25, 0.6, 0.32, 0.25)
-b = a.schedule('RUN', 0, True, 1440, 4, 0.25, 0.6, 0.32, 0.25)
-b = a.schedule('RUN', 0, True, 1440, 4, 0.25, 0.6, 0.32, 0.25)
+
+a = SIMULATE()
+b = a.schedule('INIT', 0, 1440, 4, 0.25, 0.6, 0.32, 0.25)
+b = a.schedule('RUN', 0, 1440, 4, 0.25, 0.6, 0.32, 0.25)
+b = a.schedule('RUN', 0, 1440, 4, 0.25, 0.6, 0.32, 0.25)
+b = a.schedule('RUN', 0, 1440, 4, 0.25, 0.6, 0.32, 0.25)
+b = a.schedule('RUN', 0, 1440, 4, 0.25, 0.6, 0.32, 0.25)
+b = a.schedule('RUN_SIM', 0, 1440, 4, 0.25, 0.6, 0.32, 0.25)
+print(b)
 
 """
+
+	
--- simulation.py
+++ simulation.py
@@ -1,5 +1,4 @@
 import argparse
-from simantha import System, Machine, Source, Buffer, Sink
 import numpy as np
 
 cycle_times = [0.25, 0.6, 0.32, 0.25]
@@ -8,37 +7,41 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-c', '--cycle_times', nargs='*', default=[], type=float, help='Machines cycle times')
 parser.add_argument('-s', '--simulation_time', default=1440, type=float, help='Simulation time')
 args = parser.parse_args()
+parsed_simulation_time = args.simulation_time
+parsed_cycle_times = args.cycle_times
 
-if len(args.cycle_times) != 0:
-    cycle_times = args.cycle_times
+if len(parsed_cycle_times) == len(cycle_times):
+    cycle_times = parsed_cycle_times
 
-S = Source()
-sink = Sink()
-M1 = Machine(name='M1', cycle_time=dict(constant=cycle_times[0]))
-M2 = Machine(name='M2', cycle_time=dict(constant=cycle_times[1]))
-M3 = Machine(name='M3', cycle_time=dict(constant=cycle_times[2]))
-M4 = Machine(name='M4', cycle_time=dict(constant=cycle_times[3]))
+def execute_simulation(cycle_times, parsed_simulation_time):
+    from simantha import System, Machine, Source, Buffer, Sink
+    S = Source()
+    sink = Sink()
+    M1 = Machine(name='M1', cycle_time=dict(constant=cycle_times[0]))
+    M2 = Machine(name='M2', cycle_time=dict(constant=cycle_times[1]))
+    M3 = Machine(name='M3', cycle_time=dict(constant=cycle_times[2]))
+    M4 = Machine(name='M4', cycle_time=dict(constant=cycle_times[3]))
 
-Queue_M1_M2 = Buffer(name='Queue_M1_M2', capacity=10)
-Queue_M2_M3 = Buffer(name='Queue_M2_M3', capacity=10)
-Queue_M3_M4 = Buffer(name='Queue_M3_M4', capacity=10)
+    Queue_M1_M2 = Buffer(name='Queue_M1_M2', capacity=10)
+    Queue_M2_M3 = Buffer(name='Queue_M2_M3', capacity=10)
+    Queue_M3_M4 = Buffer(name='Queue_M3_M4', capacity=10)
 
-S.define_routing(downstream=[M1])
+    S.define_routing(downstream=[M1])
 
-M1.define_routing(upstream=[S], downstream=[Queue_M1_M2])
-M2.define_routing(upstream=[Queue_M1_M2], downstream=[Queue_M2_M3])
-M3.define_routing(upstream=[Queue_M2_M3], downstream=[Queue_M3_M4])
-M4.define_routing(upstream=[Queue_M3_M4], downstream=[sink])
+    M1.define_routing(upstream=[S], downstream=[Queue_M1_M2])
+    M2.define_routing(upstream=[Queue_M1_M2], downstream=[Queue_M2_M3])
+    M3.define_routing(upstream=[Queue_M2_M3], downstream=[Queue_M3_M4])
+    M4.define_routing(upstream=[Queue_M3_M4], downstream=[sink])
 
-Queue_M1_M2.define_routing(upstream=[M1], downstream=[M2])
-Queue_M2_M3.define_routing(upstream=[M2], downstream=[M3])
-Queue_M3_M4.define_routing(upstream=[M3], downstream=[M4])
+    Queue_M1_M2.define_routing(upstream=[M1], downstream=[M2])
+    Queue_M2_M3.define_routing(upstream=[M2], downstream=[M3])
+    Queue_M3_M4.define_routing(upstream=[M3], downstream=[M4])
 
-objects = [S, sink] + [M1, M2, M3, M4, Queue_M1_M2, Queue_M2_M3, Queue_M3_M4]
-system = System(objects=objects)
-def execute_simulation(system, args):
-    system.simulate(simulation_time=args.simulation_time, verbose=False) #1440 minutes = 1 day
-    return args.simulation_time / sum(sink.level for sink in system.sinks) #returns the throughput time
+    objects = [S, sink] + [M1, M2, M3, M4, Queue_M1_M2, Queue_M2_M3, Queue_M3_M4]
+    system = System(objects=objects)
+    system.simulate(simulation_time=parsed_simulation_time, verbose=False)
+
+    return parsed_simulation_time / sum(sink.level for sink in system.sinks)
 
 if __name__ == '__main__':
-    print('Throughput time: ', execute_simulation(system, args))
+    print('Throughput time: ', execute_simulation(cycle_times, parsed_simulation_time))
